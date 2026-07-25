eee5d049a0f2a7224e5d254a39dce41cc2b463fa
monarchxyspider <raffaysweet@gmail.com>
2026-07-25 19:23:30 +0500

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,1 +1,40 @@
-f
+import discord
+from discord.ext import commands
+
+
+class Utility(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="ping")
+    async def ping(self, ctx):
+        latency = round(self.bot.latency * 1000)
+
+        embed = discord.Embed(
+            title="🏓 Pong!",
+            description=f"**Bot Latency:** `{latency}ms`",
+            color=0xE53935
+        )
+
+        await ctx.send(embed=embed)
+
+    @commands.command(name="avatar")
+    async def avatar(self, ctx, member: discord.Member = None):
+        member = member or ctx.author
+
+        embed = discord.Embed(
+            title=f"{member}'s Avatar",
+            color=0xE53935
+        )
+
+        embed.set_image(url=member.display_avatar.url)
+
+        await ctx.send(embed=embed)
+
+    @commands.command(name="userinfo")
+    async def userinfo(self, ctx, member: discord.Member = None):
+        member = member or ctx.author
+
+        embed = discord.Embed(
+            title="👤 User Information",
+            color=0xE
